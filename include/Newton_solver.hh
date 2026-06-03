3ae5bbe9c773450f6c5f49aa6331d2a4b33b024a
EliasFontanari <elias.fontanari@studenti.unitn.it>
2026-06-03 22:54:37 +0200

    Solver works but it needs code optimization

--- include/Newton_solver.hh
+++ include/Newton_solver.hh
@@ -1,10 +1,15 @@
 #include <TinyAD/Scalar.hh>
 #include <iostream>
+#include <cmath>
 
 struct NewtonSolverSettings
 {
     int max_iterations = 1000;
-    double tol = 1e-6;
+    double tol = 1e-9;
+    // Armijo line search parameters
+    double gamma = 0.1;
+    double beta = 0.8;
+    double t_max = 1.0;
 };
 
 template<int N, int M>  // N = number of variables, M = number of equations
@@ -16,11 +21,11 @@ class NewtonSolver
     NewtonSolverSettings settings;
     bool success = false;
     int iterations = 0;
-    Eigen::Vector<double, N>* solution = nullptr;
-        Eigen::Matrix<AD_N, N, 1> initial_guess =
-            AD_N::make_active(Eigen::Matrix<double, N, 1>::Zero());
+    Eigen::Vector<double, N> solution = Eigen::Matrix<double, N, 1>::Zero();;
+    Eigen::Matrix<AD_N, N, 1> initial_guess =
+        AD_N::make_active(Eigen::Matrix<double, N, 1>::Zero());
     Eigen::Matrix<double, M, N>* J = nullptr;
-    Eigen::Matrix<AD_N, M, 1>* F0 = nullptr;
+    Eigen::Matrix<double, M, 1> F0 = Eigen::Matrix<double, M, 1>::Zero();       
     
     void set_settings(int max_iterations, double tol)
     {
@@ -58,6 +63,62 @@ class NewtonSolver
         this->J = new Eigen::Matrix<double, M, N>(Jac);
     }
 
+    void assign_F0(const Eigen::Matrix<AD_N, M, 1> & F0_active) 
+    {
+        for (int i = 0; i < M; ++i)
+            this->F0[i] = F0_active[i].val;
+    }
+
+   void assign_M_AD_vector(const Eigen::Matrix<AD_N, M, 1> & vec_source, Eigen::Matrix<double, M, 1> & vec_dest) 
+    {   
+        for (int i = 0; i < M; ++i)
+            vec_dest[i] = vec_source[i].val;
+    }
+
+    void assign_solution(const Eigen::Matrix<AD_N, N, 1> & sol) 
+    {   
+        for (int i = 0; i < N; ++i)
+            this->solution[i] = sol[i].val;
+    }
+
+    bool multivariate_check_minor(Eigen::Matrix<double, M, 1> & lhs , Eigen::Matrix<double, M, 1> & rhs) 
+    {
+        for (int i = 0; i < M; ++i) {
+            if (lhs[i] > rhs[i]) {
+                return false;
+            } 
+        }
+        return true; 
+    }
+
+    double merit_fun(const Eigen::Matrix<AD_N, N, 1> & x, FuncSig func) {
+        Eigen::Matrix<double, M,1> tmp;
+        assign_M_AD_vector(func(x), tmp);
+        return 0.5*pow(tmp.norm(),2);
+    }
+
+    double armijo_search(Eigen::Matrix<AD_N, N, 1> x, Eigen::Vector<double, N> d, FuncSig func) {
+        int iter = 0;
+        double t = settings.t_max;
+        bool accept_t_step = true;
+        Eigen::Matrix<double, M,1> lhs, rhs, val_at_x;
+        assign_M_AD_vector(func(x +t*d), lhs); // the sum of AD vector and standard vector gives AD vector;
+        assign_M_AD_vector(func(x) + settings.gamma * t * ((*J)*d) , rhs);
+        assign_M_AD_vector(func(x) , val_at_x);
+        Eigen::Matrix<double, N,1> merit_grad = (*J).transpose() * val_at_x;
+        // while(!multivariate_check_minor(lhs,rhs) && iter < 50) {
+        while (!(merit_fun(x+t*d,func) - merit_fun(x,func) - settings.gamma*t*merit_grad.transpose()*d <= 0)) {
+            t *= settings.beta;
+            assign_M_AD_vector(func(x + t*d),lhs); // the sum of AD vector and standarf vector gives AD vector;
+            assign_M_AD_vector(func(x) + settings.gamma * t * ((*J)*d) , rhs);
+
+            std::cout << "Current t " << t << std::endl;
+            iter += 1;
+        }
+        return t;
+    }
+
+
     void solve(FuncSig func)
     {
         if (func(initial_guess).norm() > settings.tol)
@@ -72,25 +133,29 @@ class NewtonSolver
                 // for (int i = 0; i < N; ++i)
                 //     x_double[i] = x[i].val;
                 
-                auto F0 = func(x);
-                if (F0.norm() < settings.tol)
+                auto F0_active = func(x);
+                assign_F0(F0_active);
+                if ((this->F0).norm() < settings.tol)
                 {
-                    // success = true;
-                    // solution = new Eigen::Vector<double, N>(x);
+                    success = true;
+                    assign_solution(x);
                     std::cout << "Converged in " << iterations << " iterations." << std::endl;
                     return;
                 }
-                // Jacobian(x, func);
-                // auto lu = J->fullPivLu();
-                // Eigen::Vector<double, N> d0 = -lu.solve(F0);
-                // x += d0;
+                Jacobian(x, func);
+                auto lu = J->fullPivLu();
+                Eigen::Vector<double, N> d0 = -lu.solve(F0);
+                // std::cout << "d at step " << iterations << " has value " << d0 << std::endl; 
+                double t = armijo_search(x,d0,func); 
+                x += t*d0;
+                std::cout << "t Armijo line search at step " << iterations << " has value " << t << std::endl; 
                 iterations++;
             }
         }
         else
         {
             success = true;
-            // this->solution = new Eigen::Vector<double, N>(initial_guess);
+            assign_solution(initial_guess);
         }
         
     }
--- src/AD_example.cpp
+++ src/AD_example.cpp
@@ -142,26 +142,46 @@ Eigen::Matrix<NewtonSolver<3, 2>::AD_N, 2, 1> my_func_ad(
            x * y - exp(z) + z;
     return out;
 }
-int main() {
-    NewtonSolver<3, 2> solver;
-    Eigen::Vector<double, 3> guess;
-    
-    guess << 1.0, 2.0, 0.5;
-    solver.set_initial_guess(guess);
-    solver.print_initial_guess();
-    std::cout << "Initial settings: " << solver.get_settings().max_iterations << ", " << solver.get_settings().tol << std::endl;
-    
-    solver.Jacobian(solver.initial_guess, my_func_ad);
-    if (solver.J)
-        std::cout << "Jacobian at initial guess:\n" << *solver.J << std::endl;
 
-    std::cout << "Evaluating function at initial guess:" << my_func_ad(solver.initial_guess) << std::endl;
-    solver.solve(my_func_ad);
+Eigen::Matrix<NewtonSolver<4, 4>::AD_N, 4, 1> Wood(
+        const Eigen::Matrix<NewtonSolver<4, 4>::AD_N, 4, 1>& v)
+{
+    auto x = v[0], y = v[1], z = v[2], w = v[3];
+    Eigen::Matrix<NewtonSolver<4, 4>::AD_N, 4, 1> out;
+    out << 10 * (y - pow(x,2)),
+           1-x,
+           sqrt(90) * (w - pow(z,2)),
+           1 - z;
+    return out;
+}
+
+int main() {
+    NewtonSolver<4, 4> solver;
+    Eigen::Vector<double, 4> guess;
+    
+    guess << 310.0, -8.0, 5. ,25.0;
+    solver.set_initial_guess(guess);
+
+
+    // solver.print_initial_guess();
+    // std::cout << "Initial settings: " << solver.get_settings().max_iterations << ", " << solver.get_settings().tol << std::endl;
+    
+    // solver.Jacobian(solver.initial_guess, my_func_ad);
+    // if (solver.J)
+    //     std::cout << "Jacobian at initial guess:\n" << *solver.J << std::endl;
+
+    // std::cout << "Evaluating function at initial guess:" << solver.initial_guess << std::endl;
+    solver.solve(Wood);
     if (solver.success)    {
         std::cout << "Solver converged in " << solver.iterations << " iterations." << std::endl;
-        std::cout << "Solution: " << solver.solution->transpose() << std::endl;
+        std::cout << "Solution: " << solver.solution.transpose() << std::endl;
+        std::cout << "Function evaluation at transpose " << solver.F0.transpose() << std::endl;
+
     } else {
         std::cout << "Solver did not converge after " << solver.iterations << " iterations." << std::endl;
     }
+    // auto G = my_func_ad(guess);
+    // solver.assign_F0(G);
+    // std::cout << solver.F0 << std::endl;
     return 0;
 }
